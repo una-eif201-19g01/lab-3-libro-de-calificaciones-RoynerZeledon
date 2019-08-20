d562f36a5aa5a5610b8c36ee146a11fcd7f45141
RoynerZeledon <53485045+RoynerZeledon@users.noreply.github.com>
2019-08-20 00:08:09 -0600

    Add files via upload

---
+++ Laborato3.cpp
@@ -0,0 +1,39 @@
+#include "Laborato3.h"
+int main(){
+	int s = 100;
+	string d;
+	
+	cout<<"Nombre del curso :"<<endl;
+	cin>>d;
+	LibroCalificaciones objLibro(s,d);
+	LibroCalificaciones objLibro1();
+	
+	cout<<objLibro.obtenerNotaMaxi()<<endl;
+	cout<<objLibro.obtenerReporteNotas();
+
+	int n,r=0;
+	cout<<"cuantas notas desea digitar :";
+	cin>>n;
+	
+	while(r < n){
+		int fila,columna,nota;
+		cout<<"Dijite la nota del etudiante :";
+		cin>>nota;
+		cout<<"Dijite la fila :";
+		cin>>fila;
+		
+		cout<<"Dijite columna :";
+		cin>>columna;
+		objLibro.setPos(fila,columna,nota);
+		
+		r++;
+	}
+	
+	cout<<objLibro.obtenerReporteNotas();
+	cout<<"la nota maxima :"<<objLibro.obtenerNotaMaxi()<<endl;
+	cout<<"la nota minima :"<<objLibro.obtenerNotaMni()<<endl;
+	
+	
+
+}
+
---
+++ Laborato3.h
@@ -0,0 +1,138 @@
+#ifndef LABORATO3_H
+#define LABORATO3_H
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
+
+/*
+* =====================================================================================
+*
+*       Filename:  LABORATO3
+*
+*    Description:  Loboratorio numero 3 de matrices
+*
+*
+*        Created:  19/08/2019
+*
+*         Author:  Royner Zeledon roynerjzeleonc@gmail.com
+*	Organization:  Universidad Nacional de Costa Rica
+*
+* =====================================================================================
+*/
+
+
+using namespace std;
+
+
+const static int estudiantes = 10;
+const static int examenes = 3;
+
+class LibroCalificaciones{
+	
+private:
+	string nombreCurso;
+	int calificaciones[estudiantes][examenes];
+	
+	
+public:
+	LibroCalificaciones(){
+		string nombreCurso = " ";
+		for(int i = 0; i < estudiantes; i++){
+			for(int j = 0; j < examenes; j++){
+				calificaciones[i][j] = 100;
+			}
+		}
+		
+	}
+	
+	LibroCalificaciones(int a, string n){
+		string nombreCurso = n;
+		for(int i = 0; i < estudiantes; i++){
+			for(int j = 0; j < examenes; j++){
+				calificaciones[i][j] = a;
+			}
+		}
+		
+	}
+	~LibroCalificaciones(){}
+	
+	void setPos(int f,int c,int val){
+		calificaciones[f][c] = val;
+	}
+	int getPos(int i,int j){
+		return calificaciones[i][j];
+	}
+	
+
+	int obtenerNotaMni(){
+		int nota = 100;
+		for(int i=0; i < estudiantes; i++){
+			for(int j=0; j < examenes; j++){
+				if(calificaciones[i][j]<nota){
+					nota = calificaciones[i][j];
+					
+					
+				}
+				
+			}
+		}
+		return nota;
+	}
+
+
+	int obtenerNotaMaxi(){
+		int nota = 0;
+		for(int i=0; i < estudiantes; i++){
+			for(int j=0; j < examenes; j++){
+				if(calificaciones[i][j]>nota){
+					nota = calificaciones[i][j];
+				}else{
+					return nota;
+				}
+			}
+		}
+	}
+
+
+	int obtenerPromedio(){
+		
+		int sumaNotas = 0;
+		for(int i; i < estudiantes; i++){
+			for(int j; j < examenes; j++){
+				sumaNotas = sumaNotas + calificaciones[i][j];
+			}
+		}
+		return (sumaNotas/10)/3;
+	}
+	
+	string obtenerReporteNotas(){
+		
+		stringstream s;
+		for(int i=0; i<estudiantes; i++){
+			s<<"Estudiante "<<i<<"   ";
+			for(int j=0; j<examenes; j++){
+				s<<calificaciones[i][j]<<"  ";
+			}
+			s<<endl;
+		}
+		s<<endl;
+		return s.str();
+	}
+	
+	string obtenerNotasMaxMin(){
+		
+		stringstream s;
+		s<<"nota maxima :"<<obtenerNotaMaxi();
+		s<<"nota maxima :"<<obtenerNotaMni();
+		s<<endl;
+		
+		return s.str();
+	}
+	
+	
+};
+
+#endif
+
